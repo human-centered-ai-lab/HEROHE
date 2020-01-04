ab8dc146e2b4436ee0640eb2ada5e34c01e78f0e
Robert Reihs <robert.reihs@medunigraz.at>
2020-01-04 10:32:57 +0100

    Reformating Pipline

--- herohe.py
+++ herohe.py
@@ -1,21 +1,16 @@
 import os
-
 import openslide
-
 from preprocessing.tile_image import tileSlide
 
-slideending = "mrxs"
-
-filename = "1"
-
-
-'''
+slideextension = "mrxs"
 datasetpath = "E:\HEROHE_CHALLENGE\DataSet\HEROHE_CHALLENGE"
 folderpositive = "positive"
 foldernegative = "negative"
+outputpath = "E:\HEROHE_CHALLENGE\DataSet\HEROHE_CHALLENGE"
 
+filename = "1"
 
-slide = openslide.OpenSlide(os.path.join(datasetpath, foldernegative, str(filename) + "." + slideending))
+slide = openslide.OpenSlide(os.path.join(datasetpath, foldernegative, str(filename) + "." + slideextension))
 
 print(slide.level_count)
 print(slide.dimensions)
@@ -23,7 +18,7 @@ print(slide.level_dimensions)
 print(slide.level_downsamples)
 print(slide.properties)
 print(slide.associated_images)
-'''
 
-#tileSlide(os.path.join(datasetpath, foldernegative), "E:\HEROHE_CHALLENGE\DataSet\HEROHE_CHALLENGE\\tiled", str(filename) + "." + slideending, 0)
-tileSlide("C:\work\TMP", "E:\\tiled", str(filename) + "." + slideending, 0)
+
+#tileSlide(os.path.join(datasetpath, foldernegative), "E:\HEROHE_CHALLENGE\DataSet\HEROHE_CHALLENGE\\tiled", str(filename) + "." + slideextension, 0)
+#tileSlide("C:\work\TMP", "E:\\tiled", str(filename) + "." + slideextension, 0)
